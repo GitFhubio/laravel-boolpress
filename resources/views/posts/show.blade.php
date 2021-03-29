d2fc07ec2d8b3a073ce438c249c3af1093c9e611
Fabio Petrone <fa610movic90@gmail.com>
2021-03-29 16:22:33 +0200

    struttura

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,14 +1,18 @@
 
 @extends('layouts.app')
 @section('content')
+<div style="padding:20px;" class="post">
+<h1>Post-title: {{$post->title}}</h1>
+<p>{{$post->body}}</p>
+</div>
 {{-- <a class="btn btn-primary" href="{{route('comments.create')}}">Crea</a> --}}
 <table class="table table-striped">
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">Comment_Body</th>
-        <th scope="col">Comment_Likes</th>
-        <th scope="col">Back_To_Post</th>
+        <th scope="col">Comments</th>
+        <th scope="col">Likes</th>
+        <th scope="col">Back_To_Posts</th>
       </tr>
     </thead>
     <tbody>
@@ -17,7 +21,7 @@
             <th scope="row">{{$comment->id}}</th>
             <td> {{$comment->body}}</td>
             <td> {{$comment->likes}}</td>
-            <td><a class="btn btn-primary" href="{{route('posts.index')}}">Back to post</a></td>
+            <td><a class="btn btn-primary" href="{{route('posts.index')}}">Back to posts</a></td>
           </tr>
         @endforeach
 
