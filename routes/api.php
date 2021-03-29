6ba9f5c4dbb8874cc86ff398eae04b098b79aa31
Fabio Petrone <fa610movic90@gmail.com>
2021-03-29 19:17:49 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('fakecomments/{id}','Api\CommentsController@create')->name('myapi');
-Route::post('fakecomments/{id}','Api\CommentsController@create')->name('myapi');
+Route::get('fakecomments/{id}','Api\CommentsController@create');
+Route::post('fakecomments/{id}','Api\CommentsController@create');
